2f1a0c86989948dcb23f0bc285e2c3065b32cb7c
naomitr <52180609+naomitr@users.noreply.github.com>
2019-06-27 15:51:16 -0700

    Add files via upload

---
+++ First_Python.py
@@ -0,0 +1,10 @@
+# How to comment hehehe
+
+print("Hello, World! And welcome to my YouTube Channel! "
+"Today I'll be unboxing a python!!"
+" Woah!"s
+" I've heard that this python is 20 feet long and it's middle is four inches thicc!"
+" That's a lot of damage!")
+
+answer = input("What is your name! I wish I was named" answer,"!")
+print("It's a nice name")
